62e3e522e1ecf7024f35cdf5e3fe7dfe17d46852
k12re <83732570+k12re@users.noreply.github.com>
2022-08-23 21:56:43 +0200

    added alternate solutions

--- script.js
+++ script.js
@@ -18,3 +18,33 @@ fetch(url)
   .catch(function (error) {
     console.log(error);
   });
+
+//Alternate solution with reusable function:
+
+function getCatFacts() {
+  fetch(url)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (results) {
+      console.log(results.length);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
+getCatFacts();
+
+//Alternate solution with arrow functions
+
+function getCatFacts() {
+  fetch(url)
+    .then((response) => response.json())
+    .then((results) => {
+      console.log(results.length);
+    })
+    .catch((error) => console.log(error));
+}
+
+getCatFacts();
