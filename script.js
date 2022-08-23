58fe16f1422b675ddee5df1de8287b4403c4a5f0
k12re <83732570+k12re@users.noreply.github.com>
2022-08-23 21:40:33 +0200

    did q1-q2

---
+++ script.js
@@ -0,0 +1,22 @@
+//Question 1
+
+const greet = (name) => {
+  return `Hello ${name}`;
+};
+
+const add = (a, b) => a + b;
+
+//Question 2
+
+const url = "https://noroff.herokuapp.com/v1/cat-facts/facts";
+
+fetch(url)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (results) {
+    console.log(results.length);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
